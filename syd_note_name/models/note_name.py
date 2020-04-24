7b8b9aa26e0ff9a412723a9453b0ec17348a75a6
Lucio <lucio.valente@infosons.com>
2020-04-24 10:11:08 +0200

    note name on quick create

--- syd_note_name/models/note_name.py
+++ syd_note_name/models/note_name.py
@@ -6,3 +6,11 @@ class Note(models.Model):
     _inherit = ['note.note']
     
     name = fields.Char('Name')
+    
+    
+    @api.constrains('memo')
+    def _generate_name(self):
+        """ Read the first line of the memo to determine the note name """
+        for note in self:
+            if not note.name:
+                super(Note,self)._compute_name()
